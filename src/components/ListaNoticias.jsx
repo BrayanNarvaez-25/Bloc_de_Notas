47536e0916bee092da18381197e91c08fcb3a3a8
BrayanNarvaez-25 <brayann251107g@gmail.com>
2026-06-10 22:17:59 -0500

    BN: Paso 2 - Crear proyecto React

---
+++ src/components/ListaNoticias.jsx
@@ -0,0 +1,5 @@
+function ListaNoticias() {
+  return <div>Lista de noticias aquí</div>;
+}
+
+export default ListaNoticias;
---
+++ src/components/TarjetaNoticia.jsx
@@ -0,0 +1,5 @@
+function TarjetaNoticia({ info }) {
+  return <div>{info?.title}</div>;
+}
+
+export default TarjetaNoticia;
